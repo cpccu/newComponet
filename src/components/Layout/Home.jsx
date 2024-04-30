282bdde481e4f40f09e156ed54dde46c2372c845
rahulroynipon <rahulroynipon@gmail.com>
2024-04-30 21:47:35 +0600

    add

--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
-      <section className="lg:h-0 h-full relative bg-white">
+      <section className="lg:h-32 h-full relative bg-white">
         <EventLayout
           clName={
             " mx-2 my-14 md:mx-4 md:my-16 lg:mt-0 lg:absolute lg:left-[4rem] lg:right-[4rem]  lg:-top-[11rem] xl:left-[11rem] xl:right-[11rem] xl:-top-[11rem]"
