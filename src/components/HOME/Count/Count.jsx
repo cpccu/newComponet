b5ed20c4da61b3db58181ff1e8b03a04c0205444
rahulroynipon <rahulroynipon@gmail.com>
2024-05-04 04:20:36 +0600

    add count section

--- src/components/HOME/Count/Count.jsx
+++ src/components/HOME/Count/Count.jsx
@@ -2,29 +2,58 @@ import useIMG from "./../../../assets/icons/user.svg";
 import pictureIMG from "./../../../assets/icons/picture.svg";
 import eventIMG from "./../../../assets/icons/event.svg";
 import medalIMG from "./../../../assets/icons/medal.svg";
+import Data from "./../../../../data/Count.json";
 
 export default function Count() {
   return (
-    <main className="bg-count text-white grid md:grid-cols-2 lg:grid-cols-4 md:gap-x-5 gap-y-12 lg:gap-8 padding">
+    <main className="bg-count text-white grid  md:grid-cols-2 lg:grid-cols-4 md:gap-x-5 gap-y-14 md:gap-y-14 lg:gap-5 py-12 md:py-14 lg:py-20 padding">
       {/* users section start */}
-      <section className="grid grid-cols-2  grid-rows-2 gap-x-5">
-        <div className=" row-span-2">
-          <img src={useIMG} alt="" />
+      <section className="flex items-center justify-center gap-5">
+        <img className="h-16" src={useIMG} alt="user" />
+        <div className="flex flex-col items-start justify-center">
+          <h3 className="text-5xl font-custom font-thin text-white/90">
+            {Data?.member}
+          </h3>
+          <p className="text-xl">Members</p>
         </div>
-        <div className="row-span-1 self-end">2</div>
-        <div className="row-span-1 self-start">3</div>
       </section>
       {/* users section end */}
 
-      {/* users section start */}
-      <section className="grid grid-cols-2  grid-rows-2 gap-x-5">
-        <div className=" row-span-2">
-          <img src={useIMG} alt="" />
+      {/* picture section start */}
+      <section className="flex items-center justify-center gap-5">
+        <img className="h-16" src={pictureIMG} alt="picture" />
+        <div className="flex flex-col items-start justify-center">
+          <h3 className="text-5xl font-custom font-thin text-white/90">
+            {Data?.photo}
+          </h3>
+          <p className="text-xl">Photos</p>
         </div>
-        <div className="row-span-1 self-end">2</div>
-        <div className="row-span-1 self-start">3</div>
       </section>
-      {/* users section end */}
+      {/* picture section end */}
+
+      {/* event section start */}
+      <section className="flex items-center justify-center gap-5">
+        <img className="h-16" src={eventIMG} alt="event" />
+        <div className="flex flex-col items-start justify-center">
+          <h3 className="text-5xl font-custom font-thin text-white/90">
+            {Data?.event}
+          </h3>
+          <p className="text-xl">Events</p>
+        </div>
+      </section>
+      {/* event section end */}
+
+      {/* medal section start */}
+      <section className="flex items-center justify-center gap-5">
+        <img className="h-16" src={medalIMG} alt="medal" />
+        <div className="flex flex-col items-start justify-center">
+          <h3 className="text-5xl font-custom font-thin text-white/90">
+            {Data?.award}
+          </h3>
+          <p className="text-xl">Awards</p>
+        </div>
+      </section>
+      {/* medal section end */}
     </main>
   );
 }
