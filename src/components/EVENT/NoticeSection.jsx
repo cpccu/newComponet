671fd09460fdc09d929a0069fc1ca50ddaa8fed7
rahulroynipon <rahulroynipon@gmail.com>
2024-05-07 00:07:05 +0600

    add

--- src/components/EVENT/NoticeSection.jsx
+++ src/components/EVENT/NoticeSection.jsx
@@ -1,12 +1,17 @@
-import GalleryCard from "./../Global/GalleryCard/GalleryCard";
 import UpComingEventCard from "./../HOME/eventUpcoming/UpComingEventCard";
+import Data from "./../../../data/upcomingEvent.json";
 
 export default function NoticeSection() {
   return (
-    <main className="padding">
-      <section className="bg-header text-white">
-        <UpComingEventCard />
-      </section>
+    <main className="padding py-12">
+      {Data.map((item, index) => (
+        <section
+          key={index}
+          className="bg-header text-white p-5 md:p-8 lg:p-12 my-5 md:my-8 lg:my-12"
+        >
+          <UpComingEventCard data={item} />
+        </section>
+      ))}
     </main>
   );
 }
