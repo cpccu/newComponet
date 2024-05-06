ec4cb508b488c10b125ed123bfac1c61e504c1a2
rahulroynipon <rahulroynipon@gmail.com>
2024-05-06 07:17:35 +0600

    add the our gallery section

--- src/components/HOME/GallerySection/GallerySection.jsx
+++ src/components/HOME/GallerySection/GallerySection.jsx
@@ -1,10 +1,34 @@
 import GalleryCard from "../../Global/GalleryCard/GalleryCard";
 import tabBtn from "../../../../data/home/GallerySection.json";
-import Data from "../../../../data/GallaryCard.json";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function GallerySection() {
   const [Tag, setTag] = useState("all");
+  const [Data, setData] = useState(null);
+
+  const filterData = useCallback(() => {
+    const url = "../../../../data/GallaryCard.json";
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        // Filter data based on the tag
+        const filteredData = res.filter((item) =>
+          item.tag.split(" ").includes(Tag)
+        );
+        // Set the filtered data to state
+        setData(filteredData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [Tag]); // Depend on 'tag' to trigger when the tag changes
+
+  useEffect(() => {
+    filterData();
+  }, [filterData]); // Call filterData when the component mounts or when filterData changes
 
   return (
     <section className="bg-responsibility padding py-14 md:py-20 lg:py-24">
@@ -21,7 +45,9 @@ export default function GallerySection() {
             key={index}
             className={`${index === tabBtn.element.length - 1 ? "hidden" : ""}
             ${
-              item?.tag == Tag ? "bg-header text-white" : "bg-header/15"
+              item?.tag.toLowerCase() == Tag.toLowerCase()
+                ? "bg-header text-white"
+                : "bg-header/15"
             } shrink-0 md:block px-2 md:px-6 lg:px-8 py-2  hover:bg-header hover:text-white trans`}
           >
             {item.btnText}
