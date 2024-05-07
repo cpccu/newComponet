ad245e8fc95c345787250fdd951f25b360982960
rahulroynipon <rahulroynipon@gmail.com>
2024-05-07 07:09:20 +0600

    add

---
+++ src/components/EVENT/EventHeader.jsx
@@ -0,0 +1,26 @@
+import { useCallback, useContext, useEffect } from "react";
+
+export default function EventHeader() {
+  return (
+    <header
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)), url("https://i.ibb.co/gSVbzcZ/event-BGimg.webp")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="bg-red-900 padding flex flex-col items-center justify-center py-20 md:py-24 lg:py-28 gap-4 text-white"
+    >
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold shrink-0 capitalize">
+        Event Header
+      </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, totam.
+      </p>
+
+      <button className="py-3 px-12 font-bold uppercase bg-header hover:bg-white hover:text-gray-700 trans mt-5">
+        Let's See
+      </button>
+    </header>
+  );
+}
