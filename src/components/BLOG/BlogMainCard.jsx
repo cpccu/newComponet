88b65b8ce1ced3e8cf4520f7f0bb2e543be07141
rahulroynipon <rahulroynipon@gmail.com>
2024-05-08 19:23:27 +0600

    add the card of blog

--- src/components/BLOG/BlogMainCard.jsx
+++ src/components/BLOG/BlogMainCard.jsx
@@ -1,22 +1,19 @@
-export default function BlogMainCard() {
+export default function BlogMainCard({ Data }) {
   return (
-    <main className="ring-1 shadow-xl p-5 hover:scale-110 trans">
-      <section>
-        <img
-          src="https://i.ibb.co/5xns9k3/Whats-App-Image-2024-05-05-at-14-07-05-4cebbbbf.jpg"
-          alt=""
-        />
+    <main className="bg-white p-5 hover:-translate-y-3 trans">
+      <section className="overflow-hidden h-[250px] relative">
+        <img className="h-full w-full object-cover" src={Data?.img} alt="" />
       </section>
-      <section className="flex flex-col items-start gap-3 py-3 md:py-4">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-          Lorem ipsum dolor sit amet.
-        </h1>
+      <section className="flex flex-col items-start gap-3 mt-5">
+        <h1 className="text-2xl font-semibold">Lorem ipsum dolor sit amet.</h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
           tenetur nihil mollitia maxime tempora quis repellendus exercitationem!
           Nulla, quae sequi!
         </p>
-        <button>Read more...</button>
+        <button className="px-5 py-2 mt-1 bg-header text-white hover:bg-gray-800 trans">
+          Read more
+        </button>
       </section>
     </main>
   );
--- src/components/BLOG/BlogPostLayout.jsx
+++ src/components/BLOG/BlogPostLayout.jsx
@@ -1,12 +1,12 @@
+import Data from "./../../../data/GallaryCard.json";
 import BlogMainCard from "./BlogMainCard";
 
 export default function BlogPostLayout() {
   return (
-    <main className="py-20 md:py-28 lg:py-32 padding grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-11 lg:gap-12">
-      <BlogMainCard />
-      <BlogMainCard />
-      <BlogMainCard />
-      <BlogMainCard />
+    <main className="bg-responsibility py-20 md:py-28 lg:py-32 padding grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-7">
+      {Data.map((item, index) => (
+        <BlogMainCard Data={item} />
+      ))}
     </main>
   );
 }
