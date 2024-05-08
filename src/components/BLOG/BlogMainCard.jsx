eee88180bed70bb310712b171df49034230d1382
rahulroynipon <rahulroynipon@gmail.com>
2024-05-08 20:07:04 +0600

    add the filter btn in blog

--- src/components/BLOG/BlogMainCard.jsx
+++ src/components/BLOG/BlogMainCard.jsx
@@ -1,17 +1,17 @@
 export default function BlogMainCard({ Data }) {
   return (
-    <main className="bg-white p-5 hover:-translate-y-3 trans">
+    <main className="bg-white hover:-translate-y-3 trans">
       <section className="overflow-hidden h-[250px] relative">
         <img className="h-full w-full object-cover" src={Data?.img} alt="" />
       </section>
-      <section className="flex flex-col items-start gap-3 mt-5">
+      <section className="flex flex-col items-start gap-4 p-6">
         <h1 className="text-2xl font-semibold">Lorem ipsum dolor sit amet.</h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
           tenetur nihil mollitia maxime tempora quis repellendus exercitationem!
           Nulla, quae sequi!
         </p>
-        <button className="px-5 py-2 mt-1 bg-header text-white hover:bg-gray-800 trans">
+        <button className="px-5 py-2 mt-2 bg-header text-white hover:bg-gray-800 trans">
           Read more
         </button>
       </section>
--- src/components/BLOG/BlogPostLayout.jsx
+++ src/components/BLOG/BlogPostLayout.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import Data from "./../../../data/GallaryCard.json";
 import BlogMainCard from "./BlogMainCard";
 import BlogScroll from "../../Context/BlogScroll/BlogScroll";
+import FilterBtn from "./../../../data/blog/blogFilter.json";
 
 export default function BlogPostLayout() {
   const { setBlogTarget } = useContext(BlogScroll);
@@ -11,13 +12,22 @@ export default function BlogPostLayout() {
   }, []);
 
   return (
-    <main
-      id="blogMainLayout"
-      className="bg-responsibility py-20 md:py-28 lg:py-32 px-[.5em] md:px-[1.5em] lg:px-[2.9em] xl:px-[7em] grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-7"
-    >
-      {Data.map((item, index) => (
-        <BlogMainCard Data={item} />
-      ))}
-    </main>
+    <section id="blogMainLayout" className="bg-responsibility py-12">
+      <div className="font-semibold text-sm md:text-lg flex flex-wrap items-center justify-center gap-3 md:gap-5 lg:gap-7 py-7 md:py-14">
+        {FilterBtn.map((item, index) => (
+          <button
+            key={index}
+            className={`bg-header text-white shrink-0 md:block px-2 md:px-6 lg:px-8 py-2  hover:bg-header hover:text-white trans capitalize`}
+          >
+            {item.btnText}
+          </button>
+        ))}
+      </div>
+      <main className="py-10 px-[.5em] md:px-[1.5em] lg:px-[2.9em] xl:px-[7em] grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-7">
+        {Data.map((item, index) => (
+          <BlogMainCard Data={item} />
+        ))}
+      </main>
+    </section>
   );
 }
