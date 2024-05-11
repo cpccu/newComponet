e772387abfa6b12ef27908be7ab91b18f615effa
rahulroynipon <rahulroynipon@gmail.com>
2024-05-11 22:47:57 +0600

    add

--- src/components/CONTACT/ContactMain.jsx
+++ src/components/CONTACT/ContactMain.jsx
@@ -4,44 +4,62 @@ export default function ContactMain() {
       <section className="w-full">
         <iframe
           className="w-full min-h-[500px] lg:h-full shadow-lg"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.4657030476737!2d90.30487057786988!3d23.87309942337447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c2102dc1cd51%3A0x6f95e92193fc8978!2sCity%20University%20Bangladesh!5e0!3m2!1sen!2sus!4v1715372425282!5m2!1sen!2sus"
-          allowfullscreen="true"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.4657014916597!2d90.30716657483124!3d23.873099478586848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c2102dc1cd51%3A0x6f95e92193fc8978!2sCity%20University%20Bangladesh!5e0!3m2!1sen!2sbd!4v1715442173691!5m2!1sen!2sbd"
+          allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
       </section>
       <section className="w-full border bg-white">
-        <form className="p-12 flex flex-col gap-5 shadow-lg">
+        <form className="p-5 lg:p-8 xl:p-12 flex flex-col gap-5 ">
           <section className="flex items-center gap-2">
             <div className="w-full">
-              <p className="font-semibold">Name</p>
+              <label htmlFor="name" className="font-semibold">
+                Name
+              </label>
               <input
                 className="border px-4 py-3 w-full bg-header/10 "
                 type="text"
+                id="name"
                 placeholder="Enter your name"
               />
             </div>
 
             <div className="w-full">
-              <p className="font-semibold">Email</p>
+              <label htmlFor="email" className="font-semibold">
+                Email
+              </label>
               <input
                 className="border px-4 py-3 w-full bg-header/10 "
                 type="email"
+                id="email"
                 placeholder="Enter your email"
               />
             </div>
           </section>
-          <input
-            className="px-4 py-3 border w-full bg-header/10"
-            type="text"
-            placeholder="subject"
-          />
+          <div className="w-full">
+            <label htmlFor="subject" className="font-semibold">
+              Subject
+            </label>
+            <input
+              className="px-4 py-3 border w-full bg-header/10"
+              type="text"
+              id="subject"
+              placeholder="subject"
+            />
+          </div>
 
-          <textarea
-            className="border px-4 py-3 bg-header/10"
-            rows={12}
-            placeholder="Enter your message"
-          ></textarea>
+          <div className="w-full">
+            <label htmlFor="message" className="font-semibold">
+              Message
+            </label>
+            <textarea
+              className="border px-4 py-3 bg-header/10 w-full"
+              rows={12}
+              id="message"
+              placeholder="Enter your message"
+            ></textarea>
+          </div>
         </form>
       </section>
     </main>
